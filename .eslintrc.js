ec13f39f909d21ca6c1fceb26b393d5cd3b40ae4
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2024-01-02 03:19:05 +1100

    feat(project): trying to implement yarn workspace with docker but backend project erroring.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  plugins: ["@typescript-eslint", "react", "prettier"],
-  rules: {
-    "prettier/prettier": "error",
-  },
-  settings: {
-    react: {
-      version: "detect",
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    plugins: ['@typescript-eslint', 'react', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
     },
-  },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 };
